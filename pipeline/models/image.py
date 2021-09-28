7f6d7100432c7a4d40ae35d3a70c96bc5202e4aa
smtr42 <simteiva@gmail.com>
2021-09-28 14:39:28 +0200

    Update image.py

--- pipeline/models/image.py
+++ pipeline/models/image.py
@@ -1,5 +1,9 @@
 class Image:
+    """Object containing all informations about an image.
+    It is passed on multiple pipeline tasks."""
+    
     def __init__(self, name):
+        """Multiple attributes will be added along the differents pipeline tasks."""
         self.name = name
 
     def __str__(self):
